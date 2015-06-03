300db87cfb99adea30caa592a20201eb564ed286
Alex <zchao1995@gmail.com>
2015-06-03 10:36:29 +0800

    Update 1001.cpp

--- 1000-1099/1001.cpp
+++ 1000-1099/1001.cpp
@@ -1,49 +1,49 @@
-    /*************************************************************************
-        > File Name: LightOJ1001.cpp
-        > Author: ALex
-        > Mail: zchao1995@gmail.com
-        > Created Time: 2015年06月02日 星期二 19时24分22秒
-     ************************************************************************/
-     
-    #include <functional>
-    #include <algorithm>
-    #include <iostream>
-    #include <fstream>
-    #include <cstring>
-    #include <cstdio>
-    #include <cmath>
-    #include <cstdlib>
-    #include <queue>
-    #include <stack>
-    #include <map>
-    #include <bitset>
-    #include <set>
-    #include <vector>
-     
-    using namespace std;
-     
-    const double pi = acos(-1.0);
-    const int inf = 0x3f3f3f3f;
-    const double eps = 1e-15;
-    typedef long long LL;
-    typedef pair <int, int> PLL;
-     
-    int main() {
-        int t;
-        scanf("%d", &t);
-        while (t--) {
-            int n;
-            scanf("%d", &n);
-            int a, b;
-            if (n >= 10) {
-                a = 10;
-                b = n - 10;
-            }
-            else {
-                a = 0;
-                b = n;
-            }
-            printf("%d %d\n", a, b);
+/*************************************************************************
+    > File Name: LightOJ1001.cpp
+    > Author: ALex
+    > Mail: zchao1995@gmail.com
+    > Created Time: 2015年06月02日 星期二 19时24分22秒
+ ************************************************************************/
+ 
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <map>
+#include <bitset>
+#include <set>
+#include <vector>
+ 
+using namespace std;
+ 
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;
+const double eps = 1e-15;
+typedef long long LL;
+typedef pair <int, int> PLL;
+ 
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int a, b;
+        if (n >= 10) {
+            a = 10;
+            b = n - 10;
         }
-        return 0;
+        else {
+            a = 0;
+            b = n;
+        }
+        printf("%d %d\n", a, b);
     }
+    return 0;
+}
