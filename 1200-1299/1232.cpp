bbbb4546ebbef04bc56344574c5008112ed70555
Alex <zchao1995@gmail.com>
2015-08-23 19:15:27 +0800

    Create 1232.cpp

---
+++ 1200-1299/1232.cpp
@@ -0,0 +1,68 @@
+/*************************************************************************
+    > File Name: LightOJ1232.cpp
+    > Author: ALex
+    > Mail: zchao1995@gmail.com 
+    > Created Time: 2015年08月23日 星期日 19时06分00秒
+ ************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <climits>
+#include <deque>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <utility>
+#include <sstream>
+#include <complex>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <bitset>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;
+const double eps = 1e-15;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int, int> PLL;
+const LL INF = (1LL << 60);
+
+const int mod = 100000007;
+int dp[110][10010];
+int A[110];
+
+int main() {
+	int t, icase = 1;
+	scanf("%d", &t);
+	while (t--) {
+		memset(dp, 0, sizeof(dp));
+		dp[0][0] = 1;
+		int n, k;
+		scanf("%d%d", &n, &k);
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", &A[i]);
+		}
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 0; j <= k; ++j) {
+				dp[i][j] += dp[i - 1][j];
+				dp[i][j] %= mod;
+				if (j >= A[i]) {
+					dp[i][j] += dp[i][j - A[i]];
+					dp[i][j] %= mod;
+				}
+			}
+		}
+		printf("Case %d: %d\n", icase++, dp[n][k]);
+	}
+	return 0;
+}
